a2c6620dc45281a874b079998d7d08fc4e0f7af0
rifatrahman1 <ahsan30rifat@gmail.com>
2024-09-24 14:18:08 +0600

    added to average

---
+++ average.js
@@ -0,0 +1,10 @@
+var mathematic = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalSubject = 5;
+
+    var totalMark = mathematic + biology + chemistry + physics + bangla
+    var average = totalMark / totalSubject
+     console.log(average .toFixed(2))
